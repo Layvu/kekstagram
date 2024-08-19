d4c3dcf15cebd53daa4646e0bdf433872b2aa093
Layvu <zemskyalexey.writer@mail.ru>
2024-08-19 22:17:22 +0500

    Изменил функции на стрелочные

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,17 @@
 const isLengthCorrect = (strToCheck, maxLength) => strToCheck.length <= maxLength;
 
-function isPalindrome(strToCheck) {
+const isPalindrome = (strToCheck) => {
   const currStr = strToCheck.replaceAll(' ', '').toLowerCase();
   return currStr === currStr.split('').reverse().join('');
-}
+};
 
-function extractNumbers(str) {
+const extractNumbers = (str) => {
   const result = str.toString()
     .split('')
     .map((chr) => isNaN(Number(chr)) ? '' : chr)
     .join('')
     .replaceAll(' ', '');
   return result === '' ? NaN : Number(result);
-}
+};
 
 export { isLengthCorrect, isPalindrome, extractNumbers };
